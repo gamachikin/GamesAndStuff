c7130bd0560de6a94dee3caedb7fe4a6a8f860da
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-02-02 19:00:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from "@tomphttp/bare-server-node";
+import path from 'node:path';
+import cors from 'cors';
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer('/bare/');
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'static')));
+
 const routes = [
   { path: '/', file: 'index.html' },
   { path: '/search', file: 'search.html' },
   { path: '/games', file: 'games.html' },
   { path: '/chat', file: 'chat.html' },
   { path: '/settings', file: 'settings.html' },
-];
+  ]
+
 routes.forEach((route) => {
   app.get(route.path, (req, res) => {
     res.sendFile(path.join(__dirname, 'static', route.file));
   });
+});
+
+server.on('request', (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on('listening', () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
+
+server.listen({
+  port: PORT,
 });
