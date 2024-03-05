2df8cd0acfda0535d69d227601da9ca4e14d1e43
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-03-05 08:30:13 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -16,3 +16,14 @@ if (localStorage.getItem("theme")) {
 }
 console.log(localStorage.getItem("theme"))
 
+// common.js
+function changeTabTitle(newTitle) {
+    document.title = newTitle;  // Change the title of the current tab
+}
+
+function changeFavicon(faviconUrl) {
+    var favicon = document.querySelector('link[rel="shortcut icon"]');
+    if (favicon) {
+        favicon.href = faviconUrl;  // Change the favicon of the current tab
+    }
+}
