9d7ad58aaf6032d1b4ce916238d0b33b6f2e0272
gamachikin <102493636+gamachikin@users.noreply.github.com>
2024-03-05 18:06:59 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -44,5 +44,48 @@ window.onload = function() {
         changeFavicon(storedFaviconUrl);
     }
 };
+// Function to change the tab title
+function changeTabTitle(newTitle) {
+    document.title = newTitle;  // Change the title of the current tab
+    localStorage.setItem('tabTitle', newTitle);  // Store the tab title in local storage
+}
+
+// Function to change the favicon
+function changeFavicon(faviconUrl) {
+    var favicon = document.querySelector('link[rel="shortcut icon"]');
+    if (favicon) {
+        favicon.href = faviconUrl;  // Change the favicon of the current tab
+        localStorage.setItem('faviconUrl', faviconUrl);  // Store the favicon URL in local storage
+    }
+}
+
+// Check if there are stored values for tab title and favicon and apply them
+window.onload = function() {
+    var storedTitle = localStorage.getItem('tabTitle');
+    var storedFaviconUrl = localStorage.getItem('faviconUrl');
+    
+    if (storedTitle) {
+        changeTabTitle(storedTitle);
+    }
+    
+    if (storedFaviconUrl) {
+        changeFavicon(storedFaviconUrl);
+    }
+};
+
+// Function to get the current favicon URL
+function getFaviconUrl() {
+    var favicon = document.querySelector("link[rel~='icon']");
+    return favicon ? favicon.href : '';
+}
+
+// Function to update the favicon
+function updateFavicon(iconUrl) {
+    var link = document.querySelector("link[rel~='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'icon';
+    link.href = iconUrl;
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
 
 
